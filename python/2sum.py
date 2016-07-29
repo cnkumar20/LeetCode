0c44dbbc47285437d0a272177868ece79b2cf173
cnkumar20 <cnkumar20@gmail.com>
2016-07-29 00:41:36 -0500

    algorithm practise first checkin

---
+++ python/2sum.py
@@ -0,0 +1,30 @@
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution.
+#Given nums = [2, 7, 11, 15], target = 9,
+
+#Because nums[0] + nums[1] = 2 + 7 = 9,
+#return [0, 1].
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = dict()
+        count = 0
+        for n in nums:
+            result[target-n] = count
+            count +=1
+        count = 0
+        for x in nums:
+            if(result.has_key(x))
+                resturn [result[x],count]
+            count +=1
+a = [1,2,3,4,1,4]
+ab = Solution()
+ab.twoSum(a,4)
+print(a)
+
