4b54a1f42d2153db010b4f358ef65979eae224f0
cnkumar20 <cnkumar20@gmail.com>
2016-08-28 23:20:07 -0500

    largest Number formed from array of numbers

---
+++ java/LargestNumber.java
@@ -0,0 +1,29 @@
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+		if(nums==null || nums.length==0) {
+			return "";
+		}
+		StringBuffer sb = new StringBuffer();	
+		String[] stringNum = new String[nums.length];			
+		int j = 0;		
+		for (int i : nums) {
+			stringNum[j++] = String.valueOf(i);
+		}
+		Arrays.sort(stringNum,new StringComparator());
+		for(String x : stringNum) {
+			sb.append(x);
+		}
+		if (sb.charAt(0) == '0') {
+            return "0";
+        }
+		return sb.toString();
+	}
+	public class StringComparator implements Comparator<String> {
+        @Override
+        public int compare (String str1, String str2) {
+        String ab = str1 + str2;
+        String ba = str2 + str1;
+        return ba.compareTo(ab);
+        }
+    }
+}
