4fb16427646b8fd73b259d68bc7907660455ec9c
cnkumar20 <cnkumar20@gmail.com>
2016-10-22 04:45:45 -0500

    code cleanup

---
+++ java/StringBufferVsStringBuilder.java
@@ -0,0 +1,27 @@
+import com.sun.deploy.util.StringUtils;
+
+/**
+ * Created by dexter on 10/22/16.
+ */
+public class StringBufferVsStringBuilder {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("whre are you!!!");
+        System.out.println(sb.length());
+        sb.insert(2,"kumar");
+        System.out.println(sb);
+
+        //TODO:
+        //String builder functions
+
+        StringBuilder sbuilder = new StringBuilder();
+        sbuilder.append("Kumar and his friends!!");
+
+        System.out.println(sbuilder.substring(3,9));
+
+
+    }
+
+
+}
