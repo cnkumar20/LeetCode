d26d823187f473d6ec2c8fc2ba43e2118b3813c6
cnkumar20 <cnkumar20@gmail.com>
2016-10-23 23:54:32 -0500

    check if give two trees are same

---
+++ java/SameTree.java
@@ -0,0 +1,26 @@
+/**
+ * Created by dexter on 10/23/16.
+ */
+
+
+public class SameTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public boolean isSameTree(TreeNode p,TreeNode q) {
+        if(p==null && q==null) {
+            return true;
+        }
+
+        if((p!=null && q==null) || (p==null && q!=null) || (p.val != q.val)) return false;
+
+        return  isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
+    }
+}
--- java/StringBufferVsStringBuilder.java
+++ java/StringBufferVsStringBuilder.java
@@ -4,6 +4,9 @@ import com.sun.deploy.util.StringUtils;
  * Created by dexter on 10/22/16.
  */
 public class StringBufferVsStringBuilder {
+    public boolean gmailEmail() {
+        return true;
+    }
 
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer();
