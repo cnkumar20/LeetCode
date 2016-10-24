2dea823b43bcdf7df4b1316dfede6bcc240c6e0b
cnkumar20 <cnkumar20@gmail.com>
2016-10-24 00:56:39 -0500

    max depth of the tree

---
+++ java/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+/**
+ * Created by dexter on 10/24/16.
+ */
+public class MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int iRightDepth = maxDepth(root.right);
+        int iLeftDepth = maxDepth(root.left);
+        return (iRightDepth>iLeftDepth)?iRightDepth+1:iLeftDepth+1;
+    }
+}
