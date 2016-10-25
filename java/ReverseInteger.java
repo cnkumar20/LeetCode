7c43a064ed43ddf78db92b8364b5eef9ec447d34
cnkumar20 <cnkumar20@gmail.com>
2016-10-24 20:57:06 -0500

    max depth of the tree

---
+++ java/ReverseInteger.java
@@ -0,0 +1,17 @@
+/**
+ * Created by dexter on 10/24/16.
+ */
+public class ReverseInteger {
+    public static int reverse(int num) {
+        int newValue = 0;
+        while(num !=0) {
+            newValue = newValue*10+num % 10;
+            num = num/10;
+        }
+        return newValue;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(2001));
+    }
+}
