1d91fbfffa20e1027c8d8f6bb10bdc376a719498
cnkumar20 <cnkumar20@gmail.com>
2016-10-26 02:24:39 -0500

    check if the given tree is a balanced tree

---
+++ java/BalancedTree.java
@@ -0,0 +1,16 @@
+/**
+ * Created by dexter on 10/26/16.
+ */
+public class BalancedTree {
+    public static int getHeight(SameTree.TreeNode root) {
+        if(root==null) return 0;
+        return Math.max(getHeight(root.left),getHeight(root.right))+1;
+    }
+
+    public boolean isBalanced(SameTree.TreeNode root) {
+        if(root==null) return true;
+        int heightDiff = getHeight(root.left) - getHeight(root.right);
+        if(Math.abs(heightDiff)>1) return false;
+        else return isBalanced(root.left) && isBalanced(root.right);
+    }
+}
