667707264ec45b049616a8ea4d3a7388f7b6a684
cnkumar20 <cnkumar20@gmail.com>
2016-11-01 01:56:31 -0500

    inorder traversal

---
+++ java/BFSandDFSTree.java
@@ -0,0 +1,39 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by dexter on 11/1/16.
+ */
+public class BFSandDFSTree {
+    public static void BFSTree(Tree inputQueue) {
+        Queue<Tree> buffQueue = new ArrayDeque<>();
+        if (inputQueue == null) return;
+
+        while (true) {
+            Queue<Tree> tempQueue = new ArrayDeque<>();
+
+            while (buffQueue != null) {
+
+                System.out.print(buffQueue + " ");
+                Tree temp = buffQueue.poll();
+                if (temp.left != null) {
+                    tempQueue.add(temp.left);
+                }
+                if (temp.right != null) {
+                    tempQueue.add(temp.right);
+                }
+            }
+
+            buffQueue.addAll(tempQueue);
+            System.out.println();
+
+            if (buffQueue.size() == 0)
+                break;
+
+        }
+    }
+
+
+}
