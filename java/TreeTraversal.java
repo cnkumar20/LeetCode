2b9bc06076cf41fec9f65c24e4dba8aee97db489
cnkumar20 <cnkumar20@gmail.com>
2016-11-01 00:00:20 -0500

    inorder traversal

--- java/TreeTraversal.java
+++ java/TreeTraversal.java
@@ -7,8 +7,11 @@ class TreeTraversal {
         preorder(root.left);
         preorder(root.right);
     }
+
     public void inorder(Tree root) {
     if(root==null) return;
-
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
     }
 }
