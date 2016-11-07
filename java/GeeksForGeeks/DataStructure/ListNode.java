2d4fbed3f9b0aa912490ec8cc85fdfd9c7f7b9b8
cnkumar20 <cnkumar20@gmail.com>
2016-11-06 23:10:49 -0600

    dfs search implemented

---
+++ java/GeeksForGeeks/DataStructure/ListNode.java
@@ -0,0 +1,16 @@
+package GeeksForGeeks.DataStructure;
+
+/**
+ * Created by dexter on 11/6/16.
+ */
+public class ListNode {
+    int a;
+    ListNode next;
+
+    ListNode(int a) {
+        this.a = a;
+        ListNode next = null;
+    }
+
+
+}
